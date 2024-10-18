15ee6ceca4ae9e28755cbb92ce4af00c3658fc86
Clovis Lechien <clovis.lechien@epita.fr>
2024-10-18 16:31:12 +0200

    feat(shadcn): working base for my website, will edit the components so that it fits my needs.

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,35 +1,31 @@
 import {
   Header,
-  Hero,
   About,
   Skills,
   Experience,
-  Testimonials,
   Projects,
   Contact,
   Footer
 } from '@/components/sections';
 
-import Cursor from '@/components/cursor/cursor';
-import SmoothScroll from '@/components/smooth-scroll';
+//import Cursor from '@/components/cursor/cursor';
+//import SmoothScroll from '@/components/smooth-scroll';
 
 export default function Home() {
   return (
-    <SmoothScroll>
+    /*<SmoothScroll>*/
       <div className="flex min-h-[100dvh] flex-col">
         <Header loader={true} />
         <main className="flex-1">
-          <Hero />
           <About />
           <Skills />
           <Projects />
           <Experience />
-          <Testimonials />
           <Contact />
           <Footer />
         </main>
       </div>
-      <Cursor />
-    </SmoothScroll>
+      /*<Cursor />*/
+    /*</SmoothScroll>*/
   );
 }
